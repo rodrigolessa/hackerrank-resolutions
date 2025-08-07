7d5835e6c047eb50f0e98fe8640e903ecabd3600
rodrigolessa <rodrigolsr@gmail.com>
2025-08-07 16:35:43 -0300

    Implement a linked list structure

---
+++ DSA.GetHandsDirty/DSA.MyDataStructures/MyLinkedList.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+using System.Runtime;
+
+namespace DSA.MyDataStructures;
+
+public class MyLinkedNode<T>(T value)
+{
+    public T Value { get; init; } = value;
+    public MyLinkedNode<T>? Previous { get; set; }
+    public MyLinkedNode<T>? Next { get; set; }
+}
+
+public class MyLinkedList<T>
+{
+    public MyLinkedNode<T>? Head { get; set; }
+    public MyLinkedNode<T>? Tail { get; set; }
+
+    public void AddInFirstPosition(T value)
+    {
+        var node = new MyLinkedNode<T>(value);
+        if (Head is not null)
+        {
+            node.Next = Head;
+        }
+
+        Head = node;
+        Tail ??= node;
+    }
+
+    public void AddInLastPosition(T value)
+    {
+        var node = new MyLinkedNode<T>(value);
+        if (Tail is not null)
+        {
+            Tail.Next = node;
+        }
+
+        Tail = node;
+        Head ??= node;
+    }
+
+    public T? RemoveFirstPosition()
+    {
+        if (Head is null)
+            return default;
+
+        var headValue = Head.Value;
+
+        if (Head.Next is not null)
+        {
+            Head = Head.Next;
+        }
+
+        return headValue;
+    }
+
+    public T? RemoveLastPosition()
+    {
+        if (Tail is null)
+            return default;
+
+        var tailValue = Tail.Value;
+        var currentPosition = Head;
+
+        while (currentPosition?.Next is not null)
+        {
+            if (currentPosition.Next == Tail)
+            {
+                currentPosition.Next = null;
+                Tail = currentPosition;
+                break;
+            }
+
+            currentPosition = currentPosition?.Next;
+        }
+
+        return tailValue;
+    }
+
+    public int HowMany()
+    {
+        if (Head is null)
+            return 0;
+
+        var counter = 1;
+        var currentPosition = Head;
+        while (currentPosition?.Next is not null)
+        {
+            counter++;
+            currentPosition = currentPosition.Next;
+        }
+
+        return counter;
+    }
+
+    public void Reverse()
+    {
+        if (Head is null)
+            return;
+
+        MyLinkedNode<T> currentNode = default;
+        var nextNode = Head;
+        while (nextNode is not null)
+        {
+            nextNode = Head.Next;
+            Head.Next = currentNode;
+            currentNode = Head;
+            if (nextNode is not null)
+                Head = nextNode;
+        }
+    }
+}
