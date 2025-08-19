a7caba50dab01540c1a5693d54b55023b359d166
rodrigolessa <rodrigolsr@gmail.com>
2025-08-19 08:08:23 -0300

    Add linked list feature

---
+++ DSA.GetHandsDirty/DSA.GetHandsDirty/LinkedListProblems/FindTheMiddle.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using DSA.MyDataStructures;
+
+namespace DSA.GetHandsDirty.LinkedListProblems;
+
+public static class FindTheMiddle<T>
+{
+    public static T Find(MyLinkedNode<T> head)
+    {
+        var slow = head;
+        var fast = head;
+        while (fast?.Next != null)
+        {
+            slow = slow.Next;
+            fast = fast.Next.Next;
+        }
+
+        return slow.Value;
+    }
+}
