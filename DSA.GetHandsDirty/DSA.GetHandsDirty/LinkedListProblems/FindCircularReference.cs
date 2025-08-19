b166fface1f82cd6b5fec75c8ec1c55af8404b33
rodrigolessa <rodrigolsr@gmail.com>
2025-08-19 08:58:17 -0300

    Find circular dependency

---
+++ DSA.GetHandsDirty/DSA.GetHandsDirty/LinkedListProblems/FindCircularReference.cs
@@ -0,0 +1,25 @@
+using DSA.MyDataStructures;
+
+namespace DSA.GetHandsDirty.LinkedListProblems;
+
+public static class FindCircularReference<T>
+{
+    public static bool Find(MyLinkedNode<T> head, out int steps)
+    {
+        steps = 0;
+        var slow = head;
+        var fast = head;
+        while (fast?.Next != null)
+        {
+            steps++;
+            slow = slow.Next;
+            fast = fast.Next.Next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
