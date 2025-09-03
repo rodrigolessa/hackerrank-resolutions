5e3667f507424febe9fa047bb19b50082b1b5760
rodrigolessa <rodrigolsr@gmail.com>
2025-09-03 09:26:55 -0300

    Binary tree traversals

--- DSA.GetHandsDirty/DSA.MyDataStructures.UnitTests/MyBinaryTreeTests.cs
+++ DSA.GetHandsDirty/DSA.MyDataStructures.UnitTests/MyBinaryTreeTests.cs
@@ -36,4 +36,26 @@ public class MyBinaryTreeTests
         var node = _tree.Search(n);
         ((node is not null) == expected).ShouldBeTrue();
     }
+    
+    
+    [Fact]
+    public void PreorderTraversal_GivenAValidBalanceTree_ShouldReturnAnArrayFromRootToLastAtRightmostPosition()
+    {
+        var rootAtTheBeginning = _tree.PreorderTraversal();
+        rootAtTheBeginning.ShouldBe([5,3,2,1,7,9,8,10]);
+    }
+    
+    [Fact]
+    public void InorderTraversal_GivenAValidBalanceTree_ShouldReturnAnArrayWhereRootIsAtTheMiddle()
+    {
+        var inorder = _tree.InorderTraversal();
+        inorder.ShouldBe([1,2,3,5,7,8,9,10]);
+    }
+    
+    [Fact]
+    public void PostorderTraversal_GivenAValidBalanceTree_ShouldReturnAnArrayWhereTheLastElementIsTheRoot()
+    {
+        var result = _tree.PostOrderTraversal();
+        result.ShouldBe([1,2,3,8,10,9,7,5]);
+    }
 }
--- DSA.GetHandsDirty/DSA.MyDataStructures/MyBinaryTree.cs
+++ DSA.GetHandsDirty/DSA.MyDataStructures/MyBinaryTree.cs
@@ -46,6 +46,8 @@ public class MyBinaryTree
         }
     }
 
+    #region Searching
+
     public MyBinaryNode? Search(int data)
     {
         return SearchRecursively(data, Root);
@@ -61,4 +63,86 @@ public class MyBinaryTree
 
         return SearchRecursively(data, data > currentNode.Data ? currentNode.RightChild : currentNode.LeftChild);
     }
+    
+    #endregion
+    
+    #region Traversal methods
+    
+    /// <summary>
+    /// Usar uma técnica de backtracking, passar o resultado como referência para apendar enquanto desmonta a call stack
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<int> PreorderTraversal()
+    {
+        var preorder = new List<int>();
+        if (Root is null)
+            return [];
+        
+        PreorderTraversalRecursively(Root, preorder);
+
+        return preorder;
+    }
+    
+    private void PreorderTraversalRecursively(MyBinaryNode? node, List<int> preorder)
+    {
+        if (node is null)
+            return;
+
+        preorder.Add(node.Data);
+        PreorderTraversalRecursively(node.LeftChild, preorder);
+        PreorderTraversalRecursively(node.RightChild, preorder);
+    }
+    
+    /// <summary>
+    /// If the tree is balanced the root should be at the middle of the array
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<int> InorderTraversal()
+    {
+        var inorder = new List<int>();
+        if (Root is null)
+            return [];
+        
+        InorderTraversalRecursively(Root, inorder);
+
+        return inorder;
+    }
+    
+    private void InorderTraversalRecursively(MyBinaryNode? node, List<int> inorder)
+    {
+        if (node is null)
+            return;
+        
+        InorderTraversalRecursively(node.LeftChild, inorder);
+        inorder.Add(node.Data);
+        InorderTraversalRecursively(node.RightChild, inorder);
+    }
+    
+    /// <summary>
+    /// Root is the last in the array result
+    /// A intenção é liberar a call stack o mais rápido possível
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<int> PostOrderTraversal()
+    {
+        var postorder = new List<int>();
+        if (Root is null)
+            return [];
+        
+        PostOrderTraversalRecursively(Root, postorder);
+
+        return postorder;
+    }
+    
+    private void PostOrderTraversalRecursively(MyBinaryNode? node, List<int> postorder)
+    {
+        if (node is null)
+            return;
+        
+        PostOrderTraversalRecursively(node.LeftChild, postorder);
+        PostOrderTraversalRecursively(node.RightChild, postorder);
+        postorder.Add(node.Data);
+    }
+    
+    #endregion
 }
