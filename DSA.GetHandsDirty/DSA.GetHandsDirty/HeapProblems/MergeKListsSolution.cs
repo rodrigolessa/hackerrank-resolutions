821c3844152ed7e96772b013f66e1cbddb381f3a
rodrigolessa <rodrigolsr@gmail.com>
2025-09-11 22:07:55 -0300

    Solve leet code problem merge sorted lists

---
+++ DSA.GetHandsDirty/DSA.GetHandsDirty/HeapProblems/MergeKListsSolution.cs
@@ -0,0 +1,37 @@
+using DSA.MyDataStructures.LeetCodeStructures;
+
+namespace DSA.GetHandsDirty.HeapProblems;
+
+/// <summary>
+/// LeetCode Hard Problem - 23 - https://leetcode.com/problems/merge-k-sorted-lists/description/
+/// </summary>
+public class MergeKListsSolution
+{
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+        if (lists is null || lists.Length == 0)
+            return default;
+
+        PriorityQueue<ListNode, int> queue = new PriorityQueue<ListNode, int>();
+        foreach (var node in lists)
+        {
+            if (node is null)
+                continue;
+            queue.Enqueue(node, node.val);
+        }
+
+        ListNode emptyHead = new ListNode();
+        ListNode mergedTail = emptyHead;
+        
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            mergedTail.next = node;
+            mergedTail = mergedTail.next;
+            if (node.next is not null)
+                queue.Enqueue(node.next, node.next.val);
+        }
+        
+        return emptyHead.next;
+    }
+}
