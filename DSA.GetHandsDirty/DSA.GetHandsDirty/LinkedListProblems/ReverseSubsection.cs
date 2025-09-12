c85cee93c81c4f447870bfa1f73e9370324a27c7
rodrigolessa <rodrigolsr@gmail.com>
2025-09-11 22:00:11 -0300

    Move structure to a self class

--- DSA.GetHandsDirty/DSA.GetHandsDirty/LinkedListProblems/ReverseSubsection.cs
+++ DSA.GetHandsDirty/DSA.GetHandsDirty/LinkedListProblems/ReverseSubsection.cs
@@ -1,22 +1,12 @@
+using DSA.MyDataStructures.LeetCodeStructures;
+
 namespace DSA.GetHandsDirty.LinkedListProblems;
 
 public class ReverseSubsection
 {
-    public class ListNode
-    {
-        public int Value;
-        public ListNode? Next;
-
-        public ListNode(int value = 0, ListNode? next = null)
-        {
-            this.Value = value;
-            this.Next = next;
-        }
-    }
-
     public ListNode ReverseBetween(ListNode head, int left, int right)
     {
-        if (head.Next == null || left == right || left > right)
+        if (head.next == null || left == right || left > right)
             return head;
 
         // Dummy node to simplify edge case when reversing from head
@@ -25,16 +15,16 @@ public class ReverseSubsection
 
         // Step 1: Move `previous` to node before the reversal starts
         for (var i = 1; i < left; i++)
-            nodeBeforeReversed = nodeBeforeReversed.Next!;
+            nodeBeforeReversed = nodeBeforeReversed.next!;
 
-        var current = nodeBeforeReversed.Next;
+        var current = nodeBeforeReversed.next;
         ListNode? prevReversed = null;
 
         // Step 2: Reverse the sublist from left to right
         for (var i = 0; i <= right - left; i++)
         {
-            var nextNode = current!.Next;
-            current.Next = prevReversed;
+            var nextNode = current!.next;
+            current.next = prevReversed;
             prevReversed = current;
             current = nextNode;
         }
@@ -42,9 +32,9 @@ public class ReverseSubsection
         // Step 3: Reconnect
         var nodeAfterReversed = current;
 
-        nodeBeforeReversed.Next!.Next = nodeAfterReversed;
-        nodeBeforeReversed.Next = prevReversed;
+        nodeBeforeReversed.next!.next = nodeAfterReversed;
+        nodeBeforeReversed.next = prevReversed;
 
-        return newFirstNode.Next!;
+        return newFirstNode.next!;
     }
 }
---
+++ DSA.GetHandsDirty/DSA.MyDataStructures/LeetCodeStructures/ListNode.cs
@@ -0,0 +1,15 @@
+namespace DSA.MyDataStructures.LeetCodeStructures;
+
+/// <summary>
+/// Definition for singly-linked list.
+/// </summary>
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
