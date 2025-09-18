b566535119859579ff542a8076640783c3bbbe3c
rodrigolessa <rodrigolsr@gmail.com>
2025-09-18 12:50:24 -0300

    Add anagram problems

---
+++ DSA.GetHandsDirty/DSA.GetHandsDirty/AnagramProblems/MinimumNumberOfStepsToMakeTwoStringsAnagram.cs
@@ -0,0 +1,12 @@
+namespace DSA.GetHandsDirty.AnagramProblems;
+
+/// <summary>
+/// 1347. Minimum Number of Steps to Make Two Strings Anagram
+/// </summary>
+public class MinimumNumberOfStepsToMakeTwoStringsAnagram
+{
+    public int MinSteps(string s, string t)
+    {
+        return 0;
+    }
+}
